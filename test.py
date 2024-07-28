42bff3c994663e681b7a12b8490be4dccb5bd0fb
Kishanthummar29 <127722322+Kishanthummar29@users.noreply.github.com>
2024-07-28 22:32:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -63,8 +63,9 @@ def handle_message(message):
 
         if data.get('status') == 'success':
             m3u8_link = data.get('source')
+            link29 = "https://tinyurl.com/25hxdwv2"
             bot.send_chat_action(message.chat.id, 'typing')
-            video_path = download_video(m3u8_link, video_id)
+            video_path = download_video(link29, video_id)
             if video_path:
                 bot.send_chat_action(message.chat.id, 'upload_video')
                 with open(video_path, 'rb') as video_file:
