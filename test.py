f24df34a3fd2a1e198d756170603eabab96af4ba
Kishanthummar29 <127722322+Kishanthummar29@users.noreply.github.com>
2024-07-28 23:32:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -66,7 +66,7 @@ def handle_message(message):
             bot.send_chat_action(message.chat.id, 'typing')
             #video_path = download_video(m3u8_link, video_id)
 
-        except telethon.errors.rpcerrorlist.WebpageCurlFailedError:
+        except telethon.errors.rpcerrorlist.WebpageCurlFailedError as e:
             download = await download_file(m3u8_link, video_id)
             if not download:
                 return await bot.reply_to(
